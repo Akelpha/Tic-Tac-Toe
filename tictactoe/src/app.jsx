57cc24768382bb9c5486522fe9803586d2659c70
Keren Nzira <kerennzira@gmail.com>
2025-02-20 09:57:00 +0100

    Using state and prop

--- tictactoe/src/app.jsx
+++ tictactoe/src/app.jsx
@@ -1,30 +1,32 @@
+import { useState } from "react";
+
+function Square() {
+    const [value, setValue] = useState(null);
+    function handleClick() {
+      setValue("X");
+    }
+    return <button className="justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold" onClick={handleClick}>{value}</button>;
+  }
 export default function Board() {
     return(
-        <>
-        <div className="flex flex-row w-full">
-            <button className=" justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                1</button>
-                <button className="flex flex-row justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                2</button>
-                <button className="flex flex-row justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                3</button>
+        <div className="flex justify-center items-center h-screen">
+            <div>
+                <div className="flex flex-row w-full">
+                    <Square />
+                    <Square />
+                    <Square />
+                </div>
+                <div className="flex flex-row w-full">
+                    <Square />
+                    <Square />
+                    <Square />
+                </div>
+                <div className="flex flex-row w-full">
+                    <Square />
+                    <Square />
+                    <Square />
+                </div>
+            </div>
         </div>
-        <div className="flex flex-row w-full">
-            <button className="flex flex-row justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                4</button>
-                <button className="flex flex-row justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                5</button>
-                <button className="flex flex-row justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                6</button>
-        </div>
-        <div className="flex flex-row w-full">
-            <button className="flex flex-row justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                7</button>
-                <button className="flex flex-row justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                8</button>
-                <button className="flex flex-row justify-center items-center w-16 h-16 bg-white border border-black text-4xl font-bold">
-                9</button>
-        </div>
-        </>
     )
 }
