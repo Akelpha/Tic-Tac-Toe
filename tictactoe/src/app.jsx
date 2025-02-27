fd99b9d38bbe804db093a75d1035b5ea595a9687
Keren Nzira <kerennzira@gmail.com>
2025-02-27 09:37:52 +0100

    Add game status display to Board component and clean up console log

--- tictactoe/src/app.jsx
+++ tictactoe/src/app.jsx
@@ -33,22 +33,21 @@ function Board(xIsNext, squares, onPlay) {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <div>
-          <div className="flex flex-row w-full">
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-          </div>
-          <div className="flex flex-row w-full">
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-          </div>
-          <div className="flex flex-row w-full">
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-          </div>
+        <div>{status}</div>
+        <div className="flex flex-row w-full">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="flex flex-row w-full">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="flex flex-row w-full">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
       </div>
     </>
@@ -81,7 +80,7 @@ export default function TicTacToe() {
       </li>
     );
   });
-  console.log("currentSquares:", currentSquares);
+
   return (
     <div>
       <div>
