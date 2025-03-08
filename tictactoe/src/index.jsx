6a243484170b15fae40b2bbf48bf51602ab03874
Keren Nzira <kerennzira@gmail.com>
2025-03-08 17:00:49 +0100

    Add tsparticles-engine dependency and clean up index.jsx for improved rendering of ParticlesBackground component

--- tictactoe/src/index.jsx
+++ tictactoe/src/index.jsx
@@ -1,3 +1,19 @@
+// import React, { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./style.css";
+// import App from "./App";
+// import ParticlesBackground from "./Particlesjs";
+
+// const root = createRoot(document.getElementById("root"));
+// console.log(ParticlesBackground);
+// root.render(
+//   <StrictMode>
+//     <div className="absolute top-0 left-0 w-full h-full z-[-1]">
+//       <ParticlesBackground />
+//     </div>
+//     <App />
+//   </StrictMode>
+// );
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
@@ -8,9 +24,7 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <div className="absolute top-0 left-0 w-full h-full z-[-1]">
-      <ParticlesBackground />
-    </div>
+    <ParticlesBackground />
     <App />
   </StrictMode>
 );
